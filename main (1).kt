1e833c0d1a3e906a39663a596f1d70dc72ffc812
CATATAU06 <128633595+CATATAU06@users.noreply.github.com>
2023-05-26 16:20:52 -0300

    Add files via upload

---
+++ main (1).kt
@@ -0,0 +1,20 @@
+fun main() {
+  println("Olá Mundo")
+  var nome: String = "João"
+  val nascimento: Int = 2006
+
+println(nome)          // exibe o nome
+println(nascimento)     // exibe o nascimento
+
+val myNum: Int = 5                // Int
+val myDoubleNum: Double = 8.99    // Double
+val myLetter: Char = 'J'          // Char
+val myBoolean: Boolean = false     // Boolean
+val myText: String = "Opa"      // String
+
+  println (myNum)
+  println (myDoubleNum)
+  println (myLetter)
+  println (myBoolean)
+  println (myText)
+}
