adfa115aa7fd9347170c22029ffbfbb63ae8547b
CATATAU06 <128633595+CATATAU06@users.noreply.github.com>
2023-05-29 16:47:04 -0300

    Add files via upload

---
+++ matematico.main.kt
@@ -0,0 +1,28 @@
+fun main() {
+//operadores matematicos
+var x = 12
+var y = 3
+//exibir a soma de x e y
+ var soma=x+y
+ println("A Soma é:" +soma)
+ //exibir a subtração de x e y
+ var soma=x-y
+ println("A Subtração é:" +sub)
+ //exibir a multiplicação entre x e y
+var mult=x*y
+println("A Multiplicação é:" +multi)
+//exibir a divisão entre x e y
+ var div=x/y
+ println("A divisão é:" +div)
+//resto da divisão de x por y
+ var rest=x%y
+println("O Resto da divisão é:"+rest
+
+ //incremento     
+ println("O x é:"+x)
+  ++x
+ println("Agora o x é"+x)
+ //decremento
+ --x 
+ println("Agora o x menos 1 é")
+ }
